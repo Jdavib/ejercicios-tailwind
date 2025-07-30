ad18d94d72c47c6a9b30eadfbe385ec0d5da0952
Javier Blanco <javierdblanco1@gmail.com>
2025-07-30 00:15:30 -0400

    Layaut ejercicios tailwind

---
+++ src/1-card-deathnote/micodigo.js
@@ -0,0 +1,9 @@
+let jose = document.querySelector('div');
+
+function color(){
+    jose.innerHTML = "aqui vamos hola mundo";
+    jose.style.fontSize = "100px";
+    jose.style.background = "red"; 
+
+
+}
